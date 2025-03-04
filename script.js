6e8c8c045edf83f1dfaf954f0bf65d3fd4c136a4
govindpal98 <govindpal98136@gmail.com>
2025-03-04 11:50:51 +0530

    Second Day

---
+++ script.js
@@ -0,0 +1,45 @@
+var tl = gsap.timeline();
+tl.from(".item",{
+    y:-100,
+    dutarion:1,
+    opacity:0,
+    stagger:0.25,   
+  
+})
+tl.from(".main .right",{
+    y:100,
+    dutarion:1,
+    opacity:0,  
+})
+tl.from(".main .left",{
+    x:-100,
+    dutarion:1,
+    opacity:0,  
+})
+gsap.from(".skill_pera",{
+    x:-100,
+    dutarion:1,
+    opacity:0,
+    scrollTrigger:{
+        trigger:".skill_pera",
+        start:"top 70%",
+        end:"top 30%",
+        // markers:true,
+        scrub:true
+    }
+})
+gsap.from(".icons_skill span",{
+    x:-100,
+    dutarion:1,
+    stagger:0.75,
+    opacity:0,
+    scrollTrigger:{
+        trigger:".icons_skill span",
+        start:"top 70%",
+        end:"top 30%",
+        // markers:true,
+        scrub:true
+
+    }
+})
+       
